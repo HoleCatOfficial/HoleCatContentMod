9d7ae0e2ade0b70db3f5b23dc4bf9f490195d405
HoleCat <ConstantineTheWyvern@gmail.com>
2025-11-10 21:15:02 -0500

    Minor Fix

--- Content/Equips/AuraThiefSet/AuraThiefHeadress.cs
+++ Content/Equips/AuraThiefSet/AuraThiefHeadress.cs
@@ -56,6 +56,8 @@ namespace DestroyerTest.Content.Equips.AuraThiefSet
 			{
 				Scptr.Active = true;
 			}
+			player.GetDamage(ModContent.GetInstance<ScepterClass>()) *= 0.08f;
+			ScepterClassStats.ThrowSpeedModifier = 1.75f;
 			player.buffImmune[BuffID.Frostburn] = true;
 			player.buffImmune[BuffID.Frostburn2] = true;
 			player.buffImmune[BuffID.Frozen] = true;
